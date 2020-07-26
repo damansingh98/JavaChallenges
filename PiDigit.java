5fcad7ed36389a8b7cc643b6dfe959570f43e311
Daman Singh <53314760+damansingh98@users.noreply.github.com>
2020-07-25 20:18:44 -0700

    Update PiDigit.java

--- PiDigit.java
+++ PiDigit.java
@@ -1,8 +1,8 @@
 public class PiDigit {
   public static double myPi(int num) {
-     return Math.round(Math.PI*Math.pow(10,num)) / Math.pow(10,num); #Generates
+     return Math.round(Math.PI*Math.pow(10,num)) / Math.pow(10,num); //Generates
      }
  
  public static void main(String[] args) {
-        System.out.println(myPi(3)); #Pick any int value
+        System.out.println(myPi(3)); //Pick any int value
 }
